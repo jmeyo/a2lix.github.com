0a2414031ae51689f3a754bccd02bca65eb627ae
webda2l <webda2l@gmail.com>
2013-11-30 20:14:52 +0100

    Update bootstrap to v3 final and some documentation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,3 @@
 $(function() {
-    var $window = $(window);
 
-    // side bar
-    setTimeout(function() {
-        $('.a2lix-sidenav').affix({
-            offset: {
-                top: function() {
-                    return $window.width() <= 980 ? 300 : 220
-                }
-                , bottom: 260
-            }
-        });
-    }, 100);
 });
